7751566ea99a888b3b20f020889ba63593e69818
Daniel Kavanagh <danielkavanaghwex@gmail.com>
2018-09-10 14:47:31 +0100

    adds maxChar solutions

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,40 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	let chars = {};
+	let max = 0;
+	let maxChar = '';
+
+	for (let char of str) {
+		chars[char] = chars[char] + 1 || 1;
+	}
+
+	for (let prop in chars) {
+		if (chars[prop] > max) {
+			max = chars[prop];
+			maxChar = prop;
+		}
+	}
+
+	return maxChar;
+
+}
 
 module.exports = maxChar;
+
+// function maxChar(str) {
+// 	let chars = {};
+
+// 	for (let char of str) {
+// 		chars[char] = chars[char] + 1 || 1;
+// 	}
+
+// 	return Object.keys(chars).reduce((a, b) => {
+// 		if (chars[a] > chars[b]) {
+// 			return a;
+// 		}
+
+// 		return b;
+// 	});
+// }
