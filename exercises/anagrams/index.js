f2fad06121d08efc5e54eaf18bec40fbb28dfdc3
Daniel Kavanagh <danielkavanaghwex@gmail.com>
2018-09-20 10:01:10 +0100

    adds anagrams solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,38 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+	stringA = normaliseString(stringA);
+	stringB = normaliseString(stringB);
+
+	if (stringA.length !== stringB.length) {
+		return false;
+	}
+
+	let mapA = createCharMap(stringA);
+	let mapB = createCharMap(stringB);
+
+	for (let char in mapA) {
+		if (mapA[char] !== mapB[char]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+function normaliseString(string) {
+	return string.replace(/[^\w]/g).toLowerCase();
+}
+
+function createCharMap(string) {
+	let map = {};
+
+	for (let char of string) {
+		map[char] = map[char] + 1 || 1;
+	}
+
+	return map;
+}
 
 module.exports = anagrams;
